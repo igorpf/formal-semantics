1a8ba4e1ee353a2212c981bb35a59304b740597a
Igor Ferreira <ipferreira@inf.ufrgs.br>
2016-11-08 19:23:41 -0200

    Fazendo correções conforme as sugestões do Álvaro

--- src/l1.fs
+++ src/l1.fs
@@ -2,31 +2,31 @@ open System
 module L1 = 
     
             
-   
+     
     type Ident = string
-    type TmOperator = 
-        | TmPlus
-        | TmMinus
-        | TmMult
-        | TmDiv
-        | TmLT (*<*)
-        | TmLE (*<=*)
-        | TmEQ (*==*)
-        | TmNEQ (*!=*)
-        | TmGT (*>*)
-        | TmGE (*>=*)
+    type Operator = 
+        | OpPlus
+        | OpMinus
+        | OpMult
+        | OpDiv
+        | OpLT (*<*)
+        | OpLE (*<=*)
+        | OpEQ (*==*)
+        | OpNEQ (*!=*)
+        | OpGT (*>*)
+        | OpGE (*>=*)
     type Term = 
         | TmBool of bool         
         | TmInt of int
-        | TmOp of TmOperator * Term * Term
+        | TmOp of Operator * Term * Term
         | TmIf of Term * Term * Term
         | TmX of Ident (*Variável X*)
         | TmApply of Term * Term (*e1 e2*)
-        | TmFn of Ident * Term (*fn X:T -> e*)
-        (*let*)
-        (*let rec*)
+        | TmFn of Ident * Term (*fn X:T -> e*)        
+        | TmLet of Ident * Term * Term (*let x = e1 in e2*)
+        | TmLetRec of Ident * Term * Term (*let rec f = (fn y => e1) in e2  *)
         | TmNil 
-        | TmConcat of Term * Term (*e1::e2*)
+        | TmCons of Term * Term (*e1::e2*)
         | TmIsEmpty of Term
         | TmHd of Term 
         | TmTl of Term
@@ -44,17 +44,17 @@ module L1 =
             let t1' = step t1 in
                 TmIf ( t1' , t2 , t3 )
         (*Op rules*)
-        | TmOp (TmPlus, TmInt x, TmInt y) -> TmInt (x+y)
-        | TmOp (TmMinus, TmInt x, TmInt y) -> TmInt (x-y)
-        | TmOp (TmMult, TmInt x, TmInt y) -> TmInt (x*y)
-        | TmOp (TmDiv, TmInt x, TmInt 0) -> TmRaise
-        | TmOp (TmDiv, TmInt x, TmInt y) when (y <> 0) -> TmInt (x/y)
-        | TmOp (TmLT, TmInt x, TmInt y) -> TmBool (x<y)
-        | TmOp (TmLE, TmInt x, TmInt y) -> TmBool (x<=y)
-        | TmOp (TmEQ, TmInt x, TmInt y) -> TmBool (x=y)
-        | TmOp (TmNEQ, TmInt x, TmInt y) -> TmBool (x<>y)
-        | TmOp (TmGT, TmInt x, TmInt y) -> TmBool (x>y)
-        | TmOp (TmGE, TmInt x, TmInt y) -> TmBool (x>=y)
+        | TmOp (OpPlus, TmInt x, TmInt y) -> TmInt (x+y)
+        | TmOp (OpMinus, TmInt x, TmInt y) -> TmInt (x-y)
+        | TmOp (OpMult, TmInt x, TmInt y) -> TmInt (x*y)
+        | TmOp (OpDiv, TmInt x, TmInt 0) -> TmRaise
+        | TmOp (OpDiv, TmInt x, TmInt y) when (y <> 0) -> TmInt (x/y)
+        | TmOp (OpLT, TmInt x, TmInt y) -> TmBool (x<y)
+        | TmOp (OpLE, TmInt x, TmInt y) -> TmBool (x<=y)
+        | TmOp (OpEQ, TmInt x, TmInt y) -> TmBool (x=y)
+        | TmOp (OpNEQ, TmInt x, TmInt y) -> TmBool (x<>y)
+        | TmOp (OpGT, TmInt x, TmInt y) -> TmBool (x>y)
+        | TmOp (OpGE, TmInt x, TmInt y) -> TmBool (x>=y)
         | TmOp ( t, TmInt x, t2) ->
             let t2' = step t2 in
             TmOp(t, TmInt x, t2')
@@ -62,10 +62,10 @@ module L1 =
             let t1' = step t1 in
             TmOp(t, t1', t2)
         
-        | TmHd (TmConcat (t1, t2)) -> t1
-        | TmTl (TmConcat (t1, t2)) -> t2
+        | TmHd (TmCons (t1, t2)) -> t1
+        | TmTl (TmCons (t1, t2)) -> t2
         | TmIsEmpty (TmNil) -> TmBool true
-        | TmIsEmpty (TmConcat(t,t1)) -> TmBool false
+        | TmIsEmpty (TmCons(t,t1)) -> TmBool false
 
         | TmTry (TmRaise, e2) -> e2
         (*como representar valores prontos??*)
@@ -76,5 +76,5 @@ module L1 =
         try let t' = step t
             in eval t'
             with NoRuleApplies -> t
-    let c = eval ((TmHd(TmConcat (TmInt 5, TmNil))))
+    let c = eval ((TmHd(TmCons (TmInt 5, TmNil))))
     Console.WriteLine("{0}", c)
